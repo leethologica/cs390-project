9a54301095d6857153b99de44bd07984483ccd8c
Shubham Jain <jain229@purdue.edu>
2020-12-02 18:23:38 +0530

    Finish up DQN keras for CartPole-v0

--- dqn-keras/rl-keras.py
+++ dqn-keras/rl-keras.py
@@ -1,135 +1,104 @@
 import gym
-import numpy as np
 import random
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+import os
+import numpy as np
+from collections      import deque
+from tensorflow.keras.models     import Sequential
+from tensorflow.keras.layers     import Dense
 from tensorflow.keras.optimizers import Adam
 
-from collections import deque
+class Agent():
+    def __init__(self, state_size, action_size):
+        self.weight_backup      = "cartpole_weight1.h5"
+        self.state_size         = state_size
+        self.action_size        = action_size
+        self.memory             = deque(maxlen=2000)
+        self.learning_rate      = 0.001
+        self.gamma              = 0.95
+        self.exploration_rate   = 1.0
+        self.exploration_min    = 0.01
+        self.exploration_decay  = 0.995
+        self.brain              = self._build_model()
 
-class DQN:
-    def __init__(self, env):
-        self.env     = env
-        self.memory  = deque(maxlen=2000)
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        #model.add(Dense(48, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 
-        self.gamma = 0.95
-        self.epsilon = 1.0
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
-        self.tau = 0.05
-
-        self.model        = self.create_model()
-        self.target_model = self.create_model()
-
-
-    def create_model(self):
-        model   = Sequential()
-        state_shape  = self.env.observation_space.shape
-        #model.add(Dense(24, input_dim=4, activation="relu"))
-        model.add(Dense(24, activation='relu', input_shape=(4,)))
-        #model.add(Dense(48, activation="relu"))
-        model.add(Dense(24, activation="relu"))
-        model.add(Dense(self.env.action_space.n))
-        model.compile(loss="mean_squared_error",
-            optimizer=Adam(lr=self.learning_rate))
+        if os.path.isfile(self.weight_backup):
+            model.load_weights(self.weight_backup)
+            self.exploration_rate = self.exploration_min
         return model
 
+    def save_model(self, fn):
+            self.brain.save(fn)
 
     def act(self, state):
-        if np.random.random() < self.epsilon:
-            return self.env.action_space.sample()
-        Q = self.model.predict(state)
-        return np.argmax(Q[0])
+        if np.random.rand() <= self.exploration_rate:
+            return random.randrange(self.action_size)
+        act_values = self.brain.predict(state)
+        return np.argmax(act_values[0])
 
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
-    def remember(self, state, action, reward, new_state, done):
-        self.memory.append([state.reshape((1, 4)),
-                            action,
-                            reward,
-                            new_state.reshape((1, 4)),
-                            done])
-
-
-    def replay(self):
-        batch_size = 32
-        if len(self.memory) < batch_size:
+    def replay(self, sample_batch_size):
+        if len(self.memory) < sample_batch_size:
             return
+        sample_batch = random.sample(self.memory, sample_batch_size)
+        for state, action, reward, next_state, done in sample_batch:
+            target = reward
+            if not done:
+              target = reward + self.gamma * np.amax(self.brain.predict(next_state)[0])
+            target_f = self.brain.predict(state)
+            target_f[0][action] = target
+            self.brain.fit(state, target_f, epochs=1, verbose=0)
+        if self.exploration_rate > self.exploration_min:
+            self.exploration_rate *= self.exploration_decay
 
-        samples = random.sample(self.memory, batch_size)
-        for state, action, reward, new_state, done in samples:
-            if done:
-                update = reward
-            else:
-                predictedReward = np.amax(self.model.predict(new_state)[0])
-                update = (reward + self.gamma * predictedReward)
-            Qval = self.model.predict(state)
-            Qval[0][action] = update
-            self.model.fit(state, Qval, verbose = 0)
+class CartPole:
+    def __init__(self):
+        self.sample_batch_size = 32
+        self.episodes          = 1
+        self.env               = gym.make('CartPole-v1')
 
-        self.epsilon *= self.epsilon_decay
-        self.epsilon = max(self.epsilon_min, self.epsilon)
-
-
-    def target_train(self):
-        weights = self.model.get_weights()
-        target_weights = self.target_model.get_weights()
-        for i in range(len(target_weights)):
-            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
-        self.target_model.set_weights(target_weights)
+        self.state_size        = self.env.observation_space.shape[0]
+        self.action_size       = self.env.action_space.n
+        self.agent             = Agent(self.state_size, self.action_size)
 
 
-    def save_model(self, fn):
-        self.model.save(fn)
+    def run(self):
+        try:
+            #self.env = gym.wrappers.Monitor(self.env, "examplerun", video_callable=lambda episode_id: True,force=True)
+            scores_list = []
+            for index_episode in range(self.episodes):
+                state = self.env.reset()
+                state = np.reshape(state, [1, self.state_size])
 
+                done = False
+                index = 0
+                while not done:
+                    self.env.render() #uncomment for play
 
-def main():
-    env     = gym.make("CartPole-v0")
-    env._max_episode_steps = 500
-    #gamma   = 0.9
-    #epsilon = .95
+                    action = self.agent.act(state)
 
-    #print(f"Shape of environment is {str(env.observation_space.shape[0])}")
-    #exit()
-
-    trials  = 500
-    trial_len = 250
-
-    # updateTargetNetwork = 1000
-    dqn_agent = DQN(env=env)
-    scores_arr = []
-    for trial in range(trials):
-        print(f"Performing trial {trial}")
-	    #print("Yikes!")
-        cur_state = env.reset()
-        done = False
-        score = 0
-        #for step in range(trial_len):
-        while not done:
-            #print(f"On step {trial}")
-            #env.render()
-            action = dqn_agent.act(cur_state.reshape((1, 4)))
-            new_state, reward, done, _ = env.step(action)
-
-            reward = reward if not done else -20
-            dqn_agent.remember(cur_state, action, reward, new_state, done)
-
-            #dqn_agent.replay()       # internally iterates default (prediction) model
-            #dqn_agent.target_train() # iterates target model
-
-            cur_state = new_state
-            score += 1
-        print(f"Trial {trial} has score {score}")
-        scores_arr.append(score)
-        avg_100 = 0
-        if trial >= 101:
-            list_100 = scores_arr[-100:]
-            avg_100 = sum(list_100) / 100
-            print(f"\tAverage over last 100 runs: {avg_100}")
-        dqn_agent.replay()
-    print("Saving high score")
-    dqn_agent.save_model("highscore_model")
-    print(scores_arr)
+                    next_state, reward, done, _ = self.env.step(action)
+                    next_state = np.reshape(next_state, [1, self.state_size])
+                    #self.agent.remember(state, action, reward, next_state, done) #comment for play
+                    state = next_state
+                    index += 1
+                print("Episode {}# Score: {}".format(index_episode, index + 1))
+                scores_list.append(index + 1)
+                #self.agent.replay(self.sample_batch_size) #comment for play
+        finally:
+            #self.agent.save_model("rl-temp-f3.h5")
+            print(scores_list)
+            self.env.close()
 
 if __name__ == "__main__":
-    main()
+    cartpole = CartPole()
+    cartpole.run()
